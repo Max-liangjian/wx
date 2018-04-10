773037c1d9aabd916abb1da459cb194a3437d9df
lj <526331581@qq.com>
2018-04-10 15:53:00 +0800

    *

---
+++ pages/movie/movie.js
@@ -0,0 +1,8 @@
+// page.js
+Page({
+  data: {
+    staffA: { firstName: 'Hulk', lastName: 'Hu' },
+    staffB: { firstName: 'Shang', lastName: 'You' },
+    staffC: { firstName: 'Gideon', lastName: 'Lin' }
+  }
+})
